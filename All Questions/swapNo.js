f1e07053af97ddc7c33abfa45a591e389dd6cf78
owais ali <owaisali72359@gmail.com>
2024-09-07 23:22:24 +0500

    Swapping two numbers

---
+++ All Questions/swapNo.js
@@ -0,0 +1,17 @@
+let a = 3;
+let b = 2;
+let temp;
+
+console.log(`The value before swapping is a = ${a}, b = ${b}`);
+
+temp = a;
+a = b;
+b = temp;
+
+console.log(`The value after swapping is a = ${a}, b = ${b}`);
+
+
+
+
+
+
