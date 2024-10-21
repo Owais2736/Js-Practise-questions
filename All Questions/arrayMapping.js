a681b336674c83b9d7e30f3917dc119e179c5e08
owais ali <owaisali72359@gmail.com>
2024-10-21 10:11:15 +0500

    Array Mapping

---
+++ All Questions/arrayMapping.js
@@ -0,0 +1,5 @@
+let object = [{ name: "Owais Ali", age: "21", department: "Software" }, { name: "Abdullah", age: "22", department: "CS" }, { name: "Ali", age: "32", department: "SE" }]
+
+object.map((object) => {
+    console.log(object.name, object.age, object.department);
+})
